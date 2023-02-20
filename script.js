3bd289dcc8b02359ded4d0c593fc1f2fafc93304
afrotter <jdengelbrecht97@gmail.com>
2023-02-20 21:38:25 +0200

    Fixed GET request

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-  fetch('https://loadshedding.eskom.co.za/LoadShedding/GetStatus', {
+import fetch from 'https://cdn.skypack.dev/node-fetch';
+  
+fetch('https://loadshedding.eskom.co.za/LoadShedding/GetStatus', {
     mode: 'cors'
   })
   .then(response => response.text())
   .then(text => console.log(text))
   .catch(error => console.error(error));
 
-  const fetch = require('node-fetch');
-
-fetch('https://loadshedding.eskom.co.za/LoadShedding/GetStatus')
-  .then(response => response.text())
-  .then(text => console.log(text))
-  .catch(error => console.error(error));
+console.log(text);
