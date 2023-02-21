191b14eedfb8fdfce32790673c8ce4c9310c01db
afrotter <jdengelbrecht97@gmail.com>
2023-02-21 17:08:05 +0200

    CORS fix

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import fetch from 'https://cdn.skypack.dev/node-fetch';
   
 fetch('https://loadshedding.eskom.co.za/LoadShedding/GetStatus', {
-    mode: 'no-cors'
+    
   })
   .then(response => response.text())
   .then(text => {
     const resultElement = document.getElementById('result');
-    resultElement.textContent = text;
+    resultElement.innerHTML = text;
   })
   .catch(error => console.error(error));
